35643abb0ce19b36f65b9a33b03a53adc6d9163d
Tomas Kravcik <kravcik@procorp.cz>
2025-01-31 20:28:16 +0100

    prepare stripe , getuser

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -1,14 +1,28 @@
 "use server";
 
 import User from "@/modals/user.modal";
-import { connect } from "@/db";
+import {connect} from "@/db";
 
 export async function createUser(user: any) {
-  try {
-    await connect();
-    const newUser = await User.create(user);
-    return JSON.parse(JSON.stringify(newUser));
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connect();
+        const newUser = await User.create(user);
+        return JSON.parse(JSON.stringify(newUser));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getUser(userId: string) {
+    try {
+        await connect();
+        const newUser = await User.findOne({
+            where: {
+                clerkId: userId
+            }
+        });
+        return JSON.parse(JSON.stringify(newUser));
+    }catch (error) {
+        console.log(error);
+    }
 }
