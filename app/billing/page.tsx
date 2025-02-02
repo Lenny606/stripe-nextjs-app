105a4b0876ac5e15fa2520f62aa8effed64723d6
Tomas Kravcik <kravcik@procorp.cz>
2025-02-02 10:28:19 +0100

    getuser data, default values

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -1,13 +1,26 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
-import React from "react";
+import {useState, useEffect} from "react";
+import {useRouter} from "next/navigation";
+import {getUser} from "@/actions/user.action";
 
 const ClientPage = () => {
   const { isLoaded, isSignedIn, user } = useUser();
+  const [userData , setUserData] = useState<any>(null)
+  const router = useRouter()
 
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
+  useEffect(() => {
+    async function getUserData() {
+      const userData = await getUser(user?.id || "")
+      setUserData(userData)
+    }
+
+    if (isSignedIn) {
+      getUserData()
+    }
+
+  }, [user, isSignedIn])
+
   return (
     <div className="h-full flex flex-col items-center justify-center text-2xl">
       Hello, {user?.firstName} welcome to Clerk
